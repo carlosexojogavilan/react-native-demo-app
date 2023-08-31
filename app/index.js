5e850ca0a55f7e26f9c5de84c4e494024c88c7d5
Carlos Exojo <carlosexojogavilan@gmail.com>
2023-08-31 21:03:55 +0200

    First commit, project enviroment setted and tested how to run the app

---
+++ app/index.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  NearbyJobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: COLORS.lightWhite,
+        }}
+      />
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
