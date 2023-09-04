94cba643e1fdb4dc206571d7aa49cf43ba8da122
Carlos Exojo <carlosexojogavilan@gmail.com>
2023-09-04 17:50:07 +0200

    App fully developed, left deploying it and let the repo pretty

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -16,25 +16,27 @@ import { icons, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const [activeJobType, setActiveJobType] = useState("Full-time");
   const router = useRouter();
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Welcome Adrian</Text>
+        <Text style={styles.userName}>Welcome John</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChangeText={(text) => {
+              setSearchTerm(text);
+            }}
             placeholder="What are you looking for?"
           />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
             resizeMode="contain"
